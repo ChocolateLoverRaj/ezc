776ac412abeff1aec2906de6b6169e8205abb7fe
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-09-07 17:47:52 +0000

    Timer example custom delay

--- samples/libuv/timer/main.c
+++ samples/libuv/timer/main.c
@@ -6,15 +6,22 @@ void timerCallback(uv_timer_t *handle) {
   puts("Timer callback!");
 }
 
-int main() {
+int main(int argc, char **argv) {
+  int timeout;
+  if (argc == 2) {
+    timeout = atoi(argv[1]);
+  } else {
+    timeout = 1000;
+  }
+
   uv_loop_t *loop = malloc(sizeof(uv_loop_t));
   uv_loop_init(loop);
 
   uv_timer_t timer;
   uv_timer_init(loop, &timer);
-  uv_timer_start(&timer, timerCallback, 1000, 0);
+  uv_timer_start(&timer, timerCallback, timeout, 0);
 
-  puts("Calling uv_run. Callback will be called after 1s");
+  printf("Calling uv_run. Callback will be called after %dms\n", timeout);
   uv_run(loop, UV_RUN_DEFAULT);
 
   uv_loop_close(loop);
