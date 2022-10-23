962f9961994dbc6e6b9dd31f9d041c5d0bbd7781
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-10-23 00:31:05 +0000

    Parse number literal

---
+++ lib/core/tryGetToken/__tests__/parseNumberLiteral.test.ts
@@ -0,0 +1,44 @@
+import TokenType from '../../TokenType'
+import parseNumberLiteral from '../parseNumberLiteral'
+
+test('123', async () => {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  async function * getAsyncIterable () {
+    yield '123'
+  }
+  await expect(parseNumberLiteral(getAsyncIterable())).resolves.toEqual({
+    token: {
+      type: TokenType.NUMBER_LITERAL,
+      data: 123
+    },
+    length: 3
+  })
+})
+
+test('3.0', async () => {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  async function * getAsyncIterable () {
+    yield '3.0'
+  }
+  await expect(parseNumberLiteral(getAsyncIterable())).resolves.toEqual({
+    token: {
+      type: TokenType.NUMBER_LITERAL,
+      data: 3.0
+    },
+    length: 3
+  })
+})
+
+test('-2', async () => {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  async function * getAsyncIterable () {
+    yield '-2'
+  }
+  await expect(parseNumberLiteral(getAsyncIterable())).resolves.toEqual({
+    token: {
+      type: TokenType.NUMBER_LITERAL,
+      data: -2
+    },
+    length: 2
+  })
+})
---
+++ lib/core/tryGetToken/parseNumberLiteral.ts
@@ -0,0 +1,29 @@
+import TokenType from '../TokenType'
+import charAsyncIterable from './charAsyncIterable'
+import TryGetToken from './TryGetToken'
+
+const parseNumberLiteral: TryGetToken = async stream => {
+  const iterable = charAsyncIterable(stream)
+  let number = ''
+  for await (const char of iterable) {
+    if (char === '-' && number === '') {
+      number += char
+    } else if (/\d/.test(char)) {
+      number += char
+    } else if (number.length > 0 && !number.includes('.') && char === '.') {
+      number += char
+    } else {
+      break
+    }
+  }
+  // FIXME: Doesn't really check for errors like just '-' or '3.'
+  return {
+    token: {
+      type: TokenType.NUMBER_LITERAL,
+      data: parseFloat(number)
+    },
+    length: number.length
+  }
+}
+
+export default parseNumberLiteral
