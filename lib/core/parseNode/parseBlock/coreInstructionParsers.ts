068aa7642cf2e6e6e65231438c36fb1a0d7f3a39
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-12-21 17:44:17 +0000

    parseFile can parse hello world example

--- lib/core/parseNode/parseBlock/coreInstructionParsers.ts
+++ lib/core/parseNode/parseBlock/coreInstructionParsers.ts
@@ -1,10 +1,16 @@
 import EnumItemWithData from '../../EnumItemWithData'
-import coreInput from '../parseReturnInstruction/coreInput'
+import parseAssignmentInstruction from '../parseAssignmentInstruction/parseAssignmentInstruction'
+import coreParseReturnInstructionInput from '../parseReturnInstruction/coreInput'
 import parseReturnInstruction from '../parseReturnInstruction/parseReturnInstruction'
 import TryParseNode from '../TryParseNode'
+import coreParseAssignmentInstructionInput from '../parseAssignmentInstruction/coreInput'
+import parseCallAssignable from '../parseCallAssignable/parseCallAssignable'
+import coreParseCallAssignableInput from '../parseCallAssignable/coreInput'
 
 const coreInstructionParsers: ReadonlyArray<TryParseNode<EnumItemWithData>> = [
-  parseReturnInstruction(coreInput)
+  parseReturnInstruction(coreParseReturnInstructionInput),
+  parseAssignmentInstruction(coreParseAssignmentInstructionInput),
+  parseCallAssignable(coreParseCallAssignableInput)
 ]
 
 export default coreInstructionParsers
